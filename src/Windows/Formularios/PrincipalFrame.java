07424ff687399833c3b289faa5bc18f7922c170a
Joseph Rios Henao <joseph232005@gmail.com>
2020-12-01 09:12:25 -0500

    Agregando effectos hover

--- src/Windows/Formularios/PrincipalFrame.java
+++ src/Windows/Formularios/PrincipalFrame.java
@@ -5,6 +5,7 @@
  */
 package Windows.Formularios;
 
+import java.awt.Color;
 import javax.swing.ImageIcon;
 
 /**
@@ -20,12 +21,12 @@ public class PrincipalFrame extends javax.swing.JFrame {
         initComponents();
         jLabel2.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Resources/ProfileIconDefault.png")).getImage().getScaledInstance(60, 60, java.awt.Image.SCALE_SMOOTH))); 
         jLabel1.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Resources/MenuButton.png")).getImage().getScaledInstance(60, 60, java.awt.Image.SCALE_SMOOTH))); 
-        jLabel3.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Resources/HomeIcon.png")).getImage().getScaledInstance(70, 70, java.awt.Image.SCALE_SMOOTH))); 
-        jLabel4.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Resources/CloudIcon.png")).getImage().getScaledInstance(70, 70, java.awt.Image.SCALE_SMOOTH))); 
-        jLabel5.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Resources/FilesIcon.png")).getImage().getScaledInstance(70, 70, java.awt.Image.SCALE_SMOOTH))); 
-        jLabel6.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Resources/DocumentIcon.png")).getImage().getScaledInstance(70, 70, java.awt.Image.SCALE_SMOOTH))); 
-        jLabel7.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Resources/ConfigIcon.png")).getImage().getScaledInstance(70, 70, java.awt.Image.SCALE_SMOOTH))); 
-        jLabel8.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Resources/InfoIcon.png")).getImage().getScaledInstance(70, 70, java.awt.Image.SCALE_SMOOTH))); 
+        HomeIcon.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Resources/HomeIcon.png")).getImage().getScaledInstance(70, 70, java.awt.Image.SCALE_SMOOTH))); 
+        CloudIcon.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Resources/CloudIcon.png")).getImage().getScaledInstance(70, 70, java.awt.Image.SCALE_SMOOTH))); 
+        FilesIcon.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Resources/FilesIcon.png")).getImage().getScaledInstance(70, 70, java.awt.Image.SCALE_SMOOTH))); 
+        DocumentIcon.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Resources/DocumentIcon.png")).getImage().getScaledInstance(70, 70, java.awt.Image.SCALE_SMOOTH))); 
+        ConfigIcon.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Resources/ConfigIcon.png")).getImage().getScaledInstance(70, 70, java.awt.Image.SCALE_SMOOTH))); 
+        InfoIcon.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Resources/InfoIcon.png")).getImage().getScaledInstance(70, 70, java.awt.Image.SCALE_SMOOTH))); 
 
     }
 
@@ -40,12 +41,12 @@ public class PrincipalFrame extends javax.swing.JFrame {
 
         VentanaContenedora = new javax.swing.JPanel();
         IconsBorder = new javax.swing.JPanel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
+        HomeIcon = new javax.swing.JLabel();
+        CloudIcon = new javax.swing.JLabel();
+        FilesIcon = new javax.swing.JLabel();
+        DocumentIcon = new javax.swing.JLabel();
+        ConfigIcon = new javax.swing.JLabel();
+        InfoIcon = new javax.swing.JLabel();
         BordeSuperior = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
@@ -58,30 +59,107 @@ public class PrincipalFrame extends javax.swing.JFrame {
         IconsBorder.setBackground(new java.awt.Color(97, 157, 111));
         IconsBorder.setLayout(new java.awt.GridLayout(6, 1));
 
-        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel3.setPreferredSize(new java.awt.Dimension(100, 100));
-        IconsBorder.add(jLabel3);
+        HomeIcon.setBackground(new java.awt.Color(97, 157, 111));
+        HomeIcon.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        HomeIcon.setOpaque(true);
+        HomeIcon.setPreferredSize(new java.awt.Dimension(100, 100));
+        HomeIcon.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                HomeIconMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                HomeIconMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                HomeIconMouseExited(evt);
+            }
+        });
+        IconsBorder.add(HomeIcon);
 
-        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel4.setMaximumSize(new java.awt.Dimension(0, 0));
-        jLabel4.setPreferredSize(new java.awt.Dimension(100, 100));
-        IconsBorder.add(jLabel4);
+        CloudIcon.setBackground(new java.awt.Color(97, 157, 111));
+        CloudIcon.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        CloudIcon.setOpaque(true);
+        CloudIcon.setPreferredSize(new java.awt.Dimension(100, 100));
+        CloudIcon.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                CloudIconMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                CloudIconMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                CloudIconMouseExited(evt);
+            }
+        });
+        IconsBorder.add(CloudIcon);
 
-        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel5.setPreferredSize(new java.awt.Dimension(100, 100));
-        IconsBorder.add(jLabel5);
+        FilesIcon.setBackground(new java.awt.Color(97, 157, 111));
+        FilesIcon.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        FilesIcon.setOpaque(true);
+        FilesIcon.setPreferredSize(new java.awt.Dimension(100, 100));
+        FilesIcon.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                FilesIconMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                FilesIconMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                FilesIconMouseExited(evt);
+            }
+        });
+        IconsBorder.add(FilesIcon);
 
-        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel6.setPreferredSize(new java.awt.Dimension(100, 100));
-        IconsBorder.add(jLabel6);
+        DocumentIcon.setBackground(new java.awt.Color(97, 157, 111));
+        DocumentIcon.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        DocumentIcon.setOpaque(true);
+        DocumentIcon.setPreferredSize(new java.awt.Dimension(100, 100));
+        DocumentIcon.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                DocumentIconMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                DocumentIconMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                DocumentIconMouseExited(evt);
+            }
+        });
+        IconsBorder.add(DocumentIcon);
 
-        jLabel7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel7.setPreferredSize(new java.awt.Dimension(100, 100));
-        IconsBorder.add(jLabel7);
+        ConfigIcon.setBackground(new java.awt.Color(97, 157, 111));
+        ConfigIcon.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        ConfigIcon.setOpaque(true);
+        ConfigIcon.setPreferredSize(new java.awt.Dimension(100, 100));
+        ConfigIcon.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                ConfigIconMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                ConfigIconMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                ConfigIconMouseExited(evt);
+            }
+        });
+        IconsBorder.add(ConfigIcon);
 
-        jLabel8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel8.setPreferredSize(new java.awt.Dimension(100, 100));
-        IconsBorder.add(jLabel8);
+        InfoIcon.setBackground(new java.awt.Color(97, 157, 111));
+        InfoIcon.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        InfoIcon.setOpaque(true);
+        InfoIcon.setPreferredSize(new java.awt.Dimension(100, 100));
+        InfoIcon.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                InfoIconMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                InfoIconMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                InfoIconMouseExited(evt);
+            }
+        });
+        IconsBorder.add(InfoIcon);
 
         VentanaContenedora.add(IconsBorder, java.awt.BorderLayout.LINE_START);
 
@@ -127,6 +205,93 @@ public class PrincipalFrame extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void HomeIconMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_HomeIconMouseEntered
+        // TODO add your handling code here:
+        if (HomeIcon.getBackground()!=Color.WHITE){
+            HomeIcon.setBackground(new java.awt.Color(143, 195, 155));
+        }
+    }//GEN-LAST:event_HomeIconMouseEntered
+
+    private void HomeIconMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_HomeIconMouseExited
+        // TODO add your handling code here:
+        HomeIcon.setBackground(new java.awt.Color(97,157,111));
+    }//GEN-LAST:event_HomeIconMouseExited
+
+    private void CloudIconMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_CloudIconMouseEntered
+        // TODO add your handling code here:
+        if (CloudIcon.getBackground()!=Color.WHITE){
+            CloudIcon.setBackground(new java.awt.Color(143, 195, 155));
+        }
+    }//GEN-LAST:event_CloudIconMouseEntered
+
+    private void CloudIconMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_CloudIconMouseExited
+        // TODO add your handling code here:
+        CloudIcon.setBackground(new java.awt.Color(97,157,111));
+    }//GEN-LAST:event_CloudIconMouseExited
+
+    private void FilesIconMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_FilesIconMouseEntered
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_FilesIconMouseEntered
+
+    private void FilesIconMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_FilesIconMouseExited
+        // TODO add your handling code here:
+    }//GEN-LAST:event_FilesIconMouseExited
+
+    private void DocumentIconMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_DocumentIconMouseEntered
+        // TODO add your handling code here:
+    }//GEN-LAST:event_DocumentIconMouseEntered
+
+    private void DocumentIconMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_DocumentIconMouseExited
+        // TODO add your handling code here:
+    }//GEN-LAST:event_DocumentIconMouseExited
+
+    private void ConfigIconMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ConfigIconMouseEntered
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ConfigIconMouseEntered
+
+    private void ConfigIconMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ConfigIconMouseExited
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ConfigIconMouseExited
+
+    private void InfoIconMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_InfoIconMouseEntered
+        // TODO add your handling code here:
+    }//GEN-LAST:event_InfoIconMouseEntered
+
+    private void InfoIconMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_InfoIconMouseExited
+        // TODO add your handling code here:
+    }//GEN-LAST:event_InfoIconMouseExited
+
+    private void HomeIconMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_HomeIconMouseClicked
+        // TODO add your handling code here:
+        HomeIcon.setBackground(Color.WHITE);
+    }//GEN-LAST:event_HomeIconMouseClicked
+
+    private void CloudIconMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_CloudIconMouseClicked
+        // TODO add your handling code here:
+        CloudIcon.setBackground(Color.WHITE);
+    }//GEN-LAST:event_CloudIconMouseClicked
+
+    private void FilesIconMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_FilesIconMouseClicked
+        // TODO add your handling code here:
+        FilesIcon.setBackground(Color.WHITE);
+    }//GEN-LAST:event_FilesIconMouseClicked
+
+    private void DocumentIconMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_DocumentIconMouseClicked
+        // TODO add your handling code here:
+        DocumentIcon.setBackground(Color.WHITE);
+    }//GEN-LAST:event_DocumentIconMouseClicked
+
+    private void ConfigIconMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ConfigIconMouseClicked
+        // TODO add your handling code here:
+        ConfigIcon.setBackground(Color.WHITE);
+    }//GEN-LAST:event_ConfigIconMouseClicked
+
+    private void InfoIconMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_InfoIconMouseClicked
+        // TODO add your handling code here:
+        InfoIcon.setBackground(Color.WHITE);
+    }//GEN-LAST:event_InfoIconMouseClicked
+
     /**
      * @param args the command line arguments
      */
@@ -164,16 +329,16 @@ public class PrincipalFrame extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel BordeSuperior;
+    private javax.swing.JLabel CloudIcon;
+    private javax.swing.JLabel ConfigIcon;
+    private javax.swing.JLabel DocumentIcon;
+    private javax.swing.JLabel FilesIcon;
+    private javax.swing.JLabel HomeIcon;
     private javax.swing.JPanel IconsBorder;
+    private javax.swing.JLabel InfoIcon;
     private javax.swing.JPanel PanelOne;
     private javax.swing.JPanel VentanaContenedora;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
     // End of variables declaration//GEN-END:variables
 }
